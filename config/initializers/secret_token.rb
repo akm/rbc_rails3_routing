6046d634f321eca3613e83182095bdb4dc6df1a1
akm <akima@nautilus-technologies.com>
2012-01-11 18:51:09 +0900

    $ rails new rbc_rails3_routing -T で生成しました

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RbcRails3Routing::Application.config.secret_token = 'dbdb127a9f5f09e1572facd1d16e740c7f4a831c627dbd4a28f57bdf236b13a4a5edf52b5ffa0c539b68364a57c2ea1452b881b5d5992f7ff60c51a0ce352ab6'
